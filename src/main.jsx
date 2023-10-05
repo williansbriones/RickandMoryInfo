52b2b1f08c3f85f183e7de972a8eacc77944be2e
Your Name <you@example.com>
2023-10-05 13:11:47 -0300

    ingreso de header

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Cabecera } from './moduls/app'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.Fragment>
+    <Cabecera/>
+  </React.Fragment>
+)
---
+++ src/moduls/app.jsx
@@ -0,0 +1,11 @@
+import './app.css'
+
+export function Cabecera() {
+    return(
+        <header className="Cabecera">
+            <div className='titulo-box'>
+                <h1 className='titulo'>Bienvenido a info de RaM</h1>
+            </div>
+        </header>
+    )
+}
